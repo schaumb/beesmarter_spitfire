f2ea1f16e7d00d9747ec2959d451a609075baa29
Halo Zsolt <halo.zsolt@otpmobil.com>
2016-02-12 19:37:27 +0100

    Initial Commit

---
+++ iRecognize/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iRecognize
+//
+//  Created by Háló Zsolt on 12/02/16.
+//  Copyright © 2016 Spitfire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iRecognize/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iRecognize
+//
+//  Created by Háló Zsolt on 12/02/16.
+//  Copyright © 2016 Spitfire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
